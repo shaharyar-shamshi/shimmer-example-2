079bfe812d3dabc261a75f00bc905ba21289a91e
Shaharyar Shamshi <shaharyarshamshi@gmail.com>
2024-02-15 15:09:14 +0530

    shimmer example setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Shimmer } from "react-shimmer-loader";
+import "./App.css";
 
-function App() {
+const ShimmerExample = () => {
+  const [loading, setLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      fetchRandomImage();
+    }, 3000);
+    return () => clearTimeout(delay);
+  }, []);
+
+  const fetchRandomImage = async () => {
+    try {
+      const response = await axios.get("https://source.unsplash.com/random");
+      setImageUrl(response.request.responseURL);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching image:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {loading ? (
+        <Shimmer width={300} height={300} borderRadius={"50%"} />
+      ) : (
+        <img
+          src={imageUrl}
+          alt="Random"
+          style={{ width: 300, height: 300, borderRadius: "50%" }}
+        />
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default ShimmerExample;
